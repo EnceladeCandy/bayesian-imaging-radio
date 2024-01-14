849a418c26caebf69a6c9a3842663efe88a1ab24
EnceladeCandy <noe.dia@umontreal.ca>
2024-01-14 09:46:17 -0500

    Fixed CC script for the job to generate samples with inference_sim.py

--- shell/tarp.sh
+++ shell/tarp.sh
@@ -6,20 +6,21 @@
 #SBATCH --mem=16G         # memory per node
 #SBATCH --time=00-11:59   # time (DD-HH:MM)
 #SBATCH --account=rrg-lplevass
-#SBATCH --job-name=Posterior_skirt_postneurips
+#SBATCH --job-name=Posterior_probes_postneurips
 module load python
-cd $HOME/projects/rrg-lplevass/noedia/diffusion-radio-astronomy/scripts
 source $HOME/diffusion/bin/activate
 
+# SKIRT = ncsnpp_vp_skirt_y_64_230813225149
+# PROBES = ncsnpp_probes_g_64_230604024652
 python $HOME/projects/rrg-lplevass/noedia/bayesian_imaging_radio/tarp-diffusion/scripts/inference_sim.py \
     --sigma_y=1e-2\
     --sampling_function=$HOME/projects/rrg-lplevass/data/sampling_function3.npy \
-    --prior=$HOME/projects/rrg-lplevass/data/score_models/ncsnpp_vp_skirt_y_64_230813225149 \
+    --prior=$HOME/projects/rrg-lplevass/data/score_models/ncsnpp_probes_g_64_230604024652 \
     --pad=96\
     --model_pixels=64\
     --sampler=euler\
     --num_pred=4000\
-    --num_samples=300\
-    --batch_size=600\
+    --num_samples=600\
+    --batch_size=300\
     --results_dir=/home/noedia/scratch/bayesian_imaging_radio/tarp_samples/ \
-    --experiment_name=skirt64_postneurips
+    --experiment_name=probes64_postneurips
