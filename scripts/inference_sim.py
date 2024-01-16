69d60b8182761b42aaaeaa3ae8ee83a79644f0b0
EnceladeCandy <noe.dia@umontreal.ca>
2024-01-15 21:08:33 -0500

    Code working with pc_sampler and changed test_time -> debug_mode. Added subplot title for sanity plot. Added portion to generate json file with the experiment's parameters

--- scripts/inference_sim.py
+++ scripts/inference_sim.py
@@ -11,11 +11,11 @@ This code creates an h5 file containing the ground truth, the associated posteri
 and the reconstructions obtained by forward modeling each posterior sample. 
 """
 
-
-import torch 
-import numpy as np
 import os
+import json
+import torch 
 from torch.func import vmap, grad
+import numpy as np
 from tqdm import tqdm
 import h5py
 
@@ -25,7 +25,7 @@ import sys
 sys.path.append("../models")
 
 from forward_model import model, score_likelihood, link_function
-from posterior_sampling import euler_sampler, old_pc_sampler
+from posterior_sampling import euler_sampler, old_pc_sampler, pc_sampler
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -37,7 +37,15 @@ N_WORKERS = int(os.getenv('SLURM_ARRAY_TASK_COUNT', 1))
 # defaults to one if not running under SLURM
 THIS_WORKER = int(os.getenv('SLURM_ARRAY_TASK_ID', 1))
 
+
+def create_dir(dir): 
+    if not os.path.exists(dir): 
+        os.makedirs(dir)
+
 def main(args): 
+    
+    print(f"Generating posterior samples for simulation {THIS_WORKER}/{N_WORKERS}")
+
     # Importing and loading the weights of the score of the prior 
     prior = args.prior
     score_model = ScoreModel(checkpoints_directory=prior)
@@ -46,34 +54,75 @@ def main(args):
     img_size = args.model_pixels
     pad = args.pad
 
+    # Setting the parameters for the score-based prior
     if "probes" in prior:
-        print("Running inference with probes 64*64...") 
+        print(f"Running inference with probes {img_size}*{img_size}...") 
         B, C = 1/2, 1/2
         model_parameters = (S, B, C, pad)
+        dataset = "probes"
         
-
     elif "skirt" in prior: 
-        print("Running inference with skirt...")
+        print(f"Running inference with skirt {img_size}*{img_size}...")
         B, C = 1, 0
         model_parameters = (S, B, C, pad)
-    
+        dataset = "skirt"
+
 
     # Sampling parameters
     sampler = args.sampler
     num_pred = args.num_pred
     num_corr = args.num_corr
     snr = args.snr
-    test_time = args.test_time
+    debug_mode = args.debug_mode
 
     # Number of images sampled per loop
     batch_size = args.batch_size
     num_samples = args.num_samples
     
     path = args.results_dir + f"{sampler}/"
-
+    create_dir(path)
     filename = os.path.join(path, args.experiment_name + f"_{THIS_WORKER}" + ".h5")
-
-    ground_truth = score_model.sample([1, 1, args.model_pixels, args.model_pixels], steps=num_pred)
+    
+    # ground_truth = score_model.sample([1, 1, args.model_pixels, args.model_pixels], steps=num_pred)
+    if sampler == "euler": 
+        sampling_params = num_pred
+        ground_truth = score_model.sample([1, 1, args.model_pixels, args.model_pixels], steps=sampling_params)
+    
+    elif sampler == "old_pc": 
+        sampling_params = (num_pred, num_corr, snr)
+        ground_truth = old_pc_sampler(
+            y = None, 
+            sigma_y = None, 
+            forward_model = None, 
+            score_model = score_model,
+            score_likelihood = None, 
+            model_parameters = None, 
+            num_samples = 1, 
+            pc_params = sampling_params, 
+            img_size = (img_size, img_size), 
+            keep_chain = False, 
+            debug_mode = debug_mode
+        )
+    
+    elif sampler == "pc": 
+        sampling_params = (num_pred, num_corr, snr)
+        ground_truth = pc_sampler(
+            y = None, 
+            sigma_y = None, 
+            forward_model = None, 
+            score_model = score_model,
+            score_likelihood = None, 
+            model_parameters = None, 
+            num_samples = 1, 
+            pc_params = sampling_params, 
+            img_size = (img_size, img_size), 
+            keep_chain = False, 
+            debug_mode = debug_mode
+        )
+    else : 
+                raise ValueError("The sampler specified is not implemented or does not exist. Choose between 'euler' and 'pc'")
+    
+    
     with h5py.File(filename, "w") as hf:
         hf.create_dataset("model", [args.num_samples, 1, args.model_pixels, args.model_pixels], dtype=np.float32)
 
@@ -91,7 +140,7 @@ def main(args):
         
         for i in range(int(num_samples//batch_size)):
             if sampler.lower() == "euler":
-                print("Starting posterior sampling with the Euler-Maruyama sampler...")    
+                print("Using Euler-Maruyama sampler...")    
                 samples = euler_sampler(
                     y = observation,
                     sigma_y = sigma_y,
@@ -102,10 +151,9 @@ def main(args):
                     num_samples = batch_size,
                     num_steps = num_pred,  
                     img_size = (img_size, img_size),
-                    test_time = test_time,
-                    keep_chain = False
+                    keep_chain = False,
+                    debug_mode = debug_mode
                 )
-                
 
             elif sampler.lower() == "old_pc":
                 # pc_params = [(1000, 10, 1e-2), (1000, 100, 1e-2), (1000, 1000, 1e-3)]
@@ -114,6 +162,7 @@ def main(args):
                 # pred, corr, snr = pc_params[idx]
 
                 print(f"Sampling pc pred = {num_pred}, corr = {num_corr}, snr = {snr}")
+                
                 samples = old_pc_sampler(
                     y = observation,
                     sigma_y = sigma_y, 
@@ -122,18 +171,32 @@ def main(args):
                     score_likelihood = score_likelihood, 
                     model_parameters = model_parameters,
                     num_samples = num_samples,
-                    pc_params = (num_pred, num_corr, snr),
+                    pc_params = sampling_params,
                     img_size = (img_size, img_size), 
                     keep_chain = False, 
-                    test_time= test_time 
+                    debug_mode = debug_mode 
+                )
+            
+            elif sampler.lower() == "pc": 
+                print(f"Using pc sampler: {num_pred} predictor steps| {num_corr} corrector steps| snr = {snr}")
+                sampling_params = (num_pred, num_corr, snr)
+                samples = pc_sampler(
+                    y = observation,
+                    sigma_y = sigma_y, 
+                    forward_model = model, 
+                    score_model = score_model, 
+                    score_likelihood = score_likelihood, 
+                    model_parameters = model_parameters,
+                    num_samples = num_samples,
+                    pc_params = sampling_params,
+                    img_size = (img_size, img_size), 
+                    keep_chain = False, 
+                    debug_mode = debug_mode 
                 )
                 
-                
-            else : 
-                raise ValueError("The sampler specified is not implemented or does not exist. Choose between 'euler' and 'pc'")
             
-            samples = link_function(samples, B, C)
-            hf["model"][i*batch_size: (i+1)*batch_size] = samples.cpu().numpy().astype(np.float32)
+            
+            hf["model"][i*batch_size: (i+1)*batch_size] = link_function(samples.cpu().numpy().astype(np.float32), B, C)
 
             # Let's hope it doesn't take too much time compared to the posterior sampling:
             y_hat = torch.empty(size = (batch_size, 1, img_size, img_size)).to(device)
@@ -147,13 +210,34 @@ def main(args):
             if args.sanity_plot: 
                 import matplotlib.pyplot as plt
                 fig, axs = plt.subplots(1, 2, figsize = (8, 4))
-                im = axs[0].imshow(samples[0].squeeze().cpu(), cmap = "magma")
+                im = axs[0].imshow(link_function(ground_truth[0], B, C).squeeze().cpu(), cmap = "magma")
                 plt.colorbar(im, ax = axs[0])
-                im = axs[1].imshow(ground_truth[0].squeeze().cpu(), cmap = "magma")
+                axs[0].set_title("Ground-truth")
+                im = axs[1].imshow(link_function(samples[0], B, C).squeeze().cpu(), cmap = "magma")
                 plt.colorbar(im, ax = axs[1])
+                axs[1].set_title("Posterior sample")
+                
                 plt.savefig("/home/noedia/scratch/bayesian_imaging_radio/tarp_samples/sanity_posterior.jpeg", bbox_inches = "tight")
-
-
+    
+    # Saving a json file with the script parameters 
+    if THIS_WORKER==1: 
+        print("Saving the experiment's parameters...")
+        if "ve" in str(score_model.sde): 
+            sde = "VE"
+        else: 
+            sde = "VP"
+        data = { 
+            "num_samples": num_samples,
+            "num_sims": N_WORKERS,
+            "model_pixels": img_size, 
+            "dataset": dataset,
+            "sde": sde, 
+            "experiment_name": args.experiment_name, 
+            "sampling_params": {"predictor_steps": num_pred, }
+        }
+        filename = path+ f"/{args.experiment_name}.json"
+        with open(filename, "w") as json_file: 
+            json.dump(data, json_file, indent = 2)
 
 
 if __name__ == "__main__": 
@@ -178,7 +262,7 @@ if __name__ == "__main__":
     parser.add_argument("--pad",                required = False,   default = 0,    type = int)
     parser.add_argument("--sampling_function",  required = True)
     parser.add_argument("--prior",              required = True)
-    parser.add_argument("--test_time",          required = False,   default = False,type = bool)
+    parser.add_argument("--debug_mode",          required = False,   default = False,type = bool)
     parser.add_argument("--sanity_plot",       required = False,   default = False,type = bool)
     
     args = parser.parse_args()
