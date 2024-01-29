ed800d00ba3778a21dbb9671820d72830e1f5f7b
EnceladeCandy <noe.dia@umontreal.ca>
2024-01-29 13:27:41 -0500

    To test the grid search .sh

---
+++ scripts/test_grid.py
@@ -0,0 +1,22 @@
+
+
+def main(args): 
+    predictor = args.predictor
+    corrector = args.corrector
+    snr = args.snr
+
+    print(f"{predictor} predictor steps, {corrector} corrector steps, snr ={snr}")
+
+
+
+
+
+
+if __name__ == "__main__": 
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument("--predictor", )
+    parser.add_argument("--corrector")
+    parser.add_argument("--snr")
+    args = parser.parse_args()
+    main(args)
