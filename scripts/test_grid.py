b990a8dab6e7f30c66d2b2fbf4d666de1220c20a
EnceladeCandy <noe.dia@umontreal.ca>
2024-02-01 23:48:58 -0500

    Test script for the grid search (need to delete it)

--- scripts/test_grid.py
+++ scripts/test_grid.py
@@ -8,14 +8,10 @@ def main(args):
     print(f"{predictor} predictor steps, {corrector} corrector steps, snr ={snr}")
 
 
-
-
-
-
 if __name__ == "__main__": 
     from argparse import ArgumentParser
     parser = ArgumentParser()
-    parser.add_argument("--predictor", )
+    parser.add_argument("--predictor")
     parser.add_argument("--corrector")
     parser.add_argument("--snr")
     args = parser.parse_args()
