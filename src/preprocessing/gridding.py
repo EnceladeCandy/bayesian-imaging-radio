d098aabb236fdfce637571aa20a77f6ea2dfadff
EnceladeCandy <noe.dia@umontreal.ca>
2024-05-29 09:21:47 -0400

    Still Testing

--- src/preprocessing/gridding.py
+++ src/preprocessing/gridding.py
@@ -1,13 +1,10 @@
-import sys
-sys.path.append("../../scripts/")
-import torch 
 from astropy.visualization import ImageNormalize, AsinhStretch
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 from scipy.spatial import cKDTree
 import mpol.constants as const
-
+import os
 from functools import partial
 
 
@@ -117,3 +114,76 @@ def bin_data(u, v, values, weights, bins, window_fn, truncation_radius, statisti
                         grid[j, i] = statistics_fn(value, weight)
     print(f"number of coarsened pix: {n_coarse}")
     return grid
+
+def main(args):
+    # Loading data from npz file.
+    path = args.npz_dir
+    data = np.load(path)
+
+    u = data["uu"]
+    v = data["vv"]
+    vis = data["data"]
+    weight = data["weight"]
+
+    # Hermitian augmentation:
+    uu = np.concatenate([u, -u])
+    vv = np.concatenate([v, -v])
+
+    vis_re = np.concatenate([vis.real, vis.real])
+    vis_imag = np.concatenate([vis.imag, -vis.imag])
+    weight_ = np.concatenate([weight, weight])
+
+    print(f"The measurement set contain {len(uu)} data points")
+    npix = args.npix
+    img_size = args.img_size
+    pad = (npix - img_size) // 2
+    pixel_scale = args.pixel_scale
+    u_edges, v_edges = grid(pixel_scale = pixel_scale, img_size = npix)
+    delta_u = u_edges[1] - u_edges[0] # this is delta_u, and we should probably call it that in the future
+    truncation_radius = delta_u
+
+    if args.window_function == "sinc": 
+        window_fn = partial(sinc_window, pixel_size=delta_u)
+    
+    elif args.window_function == "pillbox": 
+        window_fn = partial(pillbox_window, pixel_size=delta_u)
+    else:
+        raise ValueError("The window function specified is not implemented yet or does not exist ! Choose between 'sinc' and 'pillbox'.")
+
+    # Real part mean and count
+    params = (uu, vv, vis_re, weight_, (u_edges, v_edges), window_fn, truncation_radius)
+    vis_bin_re = bin_data(*params, statistics_fn="mean", verbose=1)
+    std_bin_re = bin_data(*params, statistics_fn="std", verbose=2)
+
+    # Image part mean
+    params = (uu, vv, vis_imag, weight_, (u_edges, v_edges), window_fn, truncation_radius)
+    vis_bin_imag = bin_data(*params, statistics_fn="mean", verbose=1)
+    std_bin_imag = bin_data(*params, statistics_fn="std", verbose=2)
+
+    # Count: 
+    counts = bin_data(*params, statistics_fn="count", verbose=1)
+
+    np.savez(
+        os.path.join(f"{args.output_dir}_gridded_{args.window_function}_{npix}.npz"),
+        vis_bin_re = vis_bin_re,
+        vis_bin_imag = vis_bin_imag,
+        std_bin_re = std_bin_re,
+        std_bin_imag = std_bin_imag,
+        counts = counts
+    )
+
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+
+    # Sampling parameters
+    parser.add_argument("--npz_dir",            required = True,                                    help = "Directory to the processed .npz measurement set")
+    parser.add_argument("--output_dir",         required = True,                                    help = "Directory where to save the gridded visibilities (call it ms_gridded.npz)")
+    parser.add_argument("--pixel_scale",        required = True, type = float,  default = 0.05,     help = "In arcsec")
+    parser.add_argument("--npix",               required = True, type = int,    default = 4096,     help = "Total number of pixels of the padded image.")
+    parser.add_argument("--window_function",    required = True, type = str,    default = "sinc",   help = "Either 'sinc' or 'pillbox'")
+    parser.add_argument("--img_size",           required = True, type = int,    default = 256,     help = "Number of pixels of the image (must be the same dimension as the dimensions of the score model)")
+    
+
+    args = parser.parse_args()
+    main(args) 
