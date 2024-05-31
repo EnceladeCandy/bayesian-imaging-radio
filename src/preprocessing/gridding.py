d7924b0b750f564779aa2026d8d80c7a088fc09c
EnceladeCandy <noe.dia@umontreal.ca>
2024-05-31 16:38:17 -0400

    Tested script to grid the visibilities in a npz file or every npz in a folder

--- src/preprocessing/gridding.py
+++ src/preprocessing/gridding.py
@@ -6,7 +6,8 @@ from scipy.spatial import cKDTree
 import mpol.constants as const
 import os
 from functools import partial
-
+from glob import glob
+import json
 
 def grid(pixel_scale, img_size): 
     """Given a pixel scale and a number of pixels in image space, grid the associated Fourier space
@@ -27,10 +28,10 @@ def grid(pixel_scale, img_size):
     dv = 1 / (img_size * dm) * 1e-3 # klambda
 
     u_min = -img_size//2 * du 
-    u_max = img_size//2 * du 
+    u_max =  img_size//2 * du 
 
     v_min = -img_size//2 * dv
-    v_max = img_size//2 * dv
+    v_max =  img_size//2 * dv
 
     u_edges = np.linspace(u_min, u_max, img_size + 1)
     v_edges = np.linspace(v_min, v_max, img_size + 1)
@@ -61,6 +62,16 @@ def sinc_window(u, center, pixel_size=0.015, m=1):
 from typing import Callable
 
 def bin_data(u, v, values, weights, bins, window_fn, truncation_radius, statistics_fn="mean", verbose=0):
+    """
+    u: u-coordinate of the data points to be aggregated
+    v: v-coordinate of the data points to be aggregated 
+    values: value at the different uv coordinates.
+    bins: grid edges
+    window_fn: Window function for the convolutional gridding
+    truncation_radius:  
+    pixel_size: Size of a pixel in the (u,v)-plane, in arcseconds
+    m: size of the truncation of this window (in term of pixel_size)
+    """
     u_edges = bins[0]
     v_edges = bins[1]
     n_coarse = 0
@@ -115,11 +126,50 @@ def bin_data(u, v, values, weights, bins, window_fn, truncation_radius, statisti
     print(f"number of coarsened pix: {n_coarse}")
     return grid
 
-def main(args):
-    # Loading data from npz file.
-    path = args.npz_dir
-    data = np.load(path)
+# total number of slurm workers detected
+# defaults to 1 if not running under SLURM
+N_WORKERS = int(os.getenv('SLURM_ARRAY_TASK_COUNT', 1))
 
+# this worker's array index. Assumes slurm array job is zero-indexed
+# defaults to one if not running under SLURM
+THIS_WORKER = int(os.getenv('SLURM_ARRAY_TASK_ID', 1))
+
+def main(args):
+    npz_dir = args.npz_dir 
+    output_dir = args.output_dir # must be a folder
+    
+    # To grid multiple datasets across nodes (npz_dir must be the path to a folder)
+    if N_WORKERS > 1: 
+        paths = glob(os.path.join(npz_dir, "*.npz"))
+        assert len(paths) > 1
+        print(f"ARRAY JOB, THIS_WORKER={THIS_WORKER}")
+        path = paths[THIS_WORKER]
+        basename = os.path.basename(path)
+        target_name, _= os.path.splitext(basename)
+        dirname = os.path.dirname(path)
+        # Importing target parameters 
+        target_params_dir = os.path.join(dirname, "dsharp_params.json")
+        with open(target_params_dir, 'r') as file:
+            data = json.load(file) 
+        pixel_scale, _, _ = data[target_name]
+
+    
+    # If gridding only one dataset (npz_dir must be the path to an npz file)
+    elif N_WORKERS == 1: 
+        paths = [args.npz_dir]
+        path = paths[0]
+        basename = os.path.basename(path)
+        target_name, _= os.path.splitext(basename)
+        dirname = os.path.dirname(path)
+        target_params_dir = os.path.join(dirname, "dsharp_params.json") # I spent some time creating this file, please use it. 
+        with open(target_params_dir, 'r') as file:
+            data = json.load(file) 
+        pixel_scale, _, _ = data[target_name] # format = (pixel_scale, hshift, wshift) 
+
+    
+    print(f"Gridding visibilities for {target_name} with pixel size = {pixel_scale:.2g}...")
+    # Importing processed DSHARP data.
+    data = np.load(path)
     u = data["uu"]
     v = data["vv"]
     vis = data["data"]
@@ -128,7 +178,6 @@ def main(args):
     # Hermitian augmentation:
     uu = np.concatenate([u, -u])
     vv = np.concatenate([v, -v])
-
     vis_re = np.concatenate([vis.real, vis.real])
     vis_imag = np.concatenate([vis.imag, -vis.imag])
     weight_ = np.concatenate([weight, weight])
@@ -136,8 +185,6 @@ def main(args):
     print(f"The measurement set contain {len(uu)} data points")
     npix = args.npix
     img_size = args.img_size
-    pad = (npix - img_size) // 2
-    pixel_scale = args.pixel_scale
     u_edges, v_edges = grid(pixel_scale = pixel_scale, img_size = npix)
     delta_u = u_edges[1] - u_edges[0] # this is delta_u, and we should probably call it that in the future
     truncation_radius = delta_u
@@ -150,40 +197,42 @@ def main(args):
     else:
         raise ValueError("The window function specified is not implemented yet or does not exist ! Choose between 'sinc' and 'pillbox'.")
 
-    # Real part mean and count
-    params = (uu, vv, vis_re, weight_, (u_edges, v_edges), window_fn, truncation_radius)
-    vis_bin_re = bin_data(*params, statistics_fn="mean", verbose=1)
-    std_bin_re = bin_data(*params, statistics_fn="std", verbose=2)
+        # Real part mean and count
+    if not args.debug_mode:
+        params = (uu, vv, vis_re, weight_, (u_edges, v_edges), window_fn, truncation_radius)
+        vis_bin_re = bin_data(*params, statistics_fn="mean", verbose=1)
+        std_bin_re = bin_data(*params, statistics_fn="std", verbose=2)
 
-    # Image part mean
-    params = (uu, vv, vis_imag, weight_, (u_edges, v_edges), window_fn, truncation_radius)
-    vis_bin_imag = bin_data(*params, statistics_fn="mean", verbose=1)
-    std_bin_imag = bin_data(*params, statistics_fn="std", verbose=2)
+        # Image part mean
+        params = (uu, vv, vis_imag, weight_, (u_edges, v_edges), window_fn, truncation_radius)
+        vis_bin_imag = bin_data(*params, statistics_fn="mean", verbose=1)
+        std_bin_imag = bin_data(*params, statistics_fn="std", verbose=2)
 
-    # Count: 
-    counts = bin_data(*params, statistics_fn="count", verbose=1)
-
-    np.savez(
-        os.path.join(f"{args.output_dir}_gridded_{args.window_function}_{npix}.npz"),
-        vis_bin_re = vis_bin_re,
-        vis_bin_imag = vis_bin_imag,
-        std_bin_re = std_bin_re,
-        std_bin_imag = std_bin_imag,
-        counts = counts
-    )
+        # Count: 
+        counts = bin_data(*params, statistics_fn="count", verbose=1)
+    
+    save_dir = os.path.join(output_dir, f"{target_name}_{npix}_gridded_{args.window_function}.npz")
+    if not args.debug_mode:
+        np.savez(
+            save_dir, 
+            vis_bin_re = vis_bin_re,
+            vis_bin_imag = vis_bin_imag,
+            std_bin_re = std_bin_re,
+            std_bin_imag = std_bin_imag,
+            counts = counts
+        )
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
     parser = ArgumentParser()
 
     # Sampling parameters
-    parser.add_argument("--npz_dir",            required = True,                                    help = "Directory to the processed .npz measurement set")
-    parser.add_argument("--output_dir",         required = True,                                    help = "Directory where to save the gridded visibilities (call it ms_gridded.npz)")
-    parser.add_argument("--pixel_scale",        required = True, type = float,  default = 0.05,     help = "In arcsec")
-    parser.add_argument("--npix",               required = True, type = int,    default = 4096,     help = "Total number of pixels of the padded image.")
-    parser.add_argument("--window_function",    required = True, type = str,    default = "sinc",   help = "Either 'sinc' or 'pillbox'")
-    parser.add_argument("--img_size",           required = True, type = int,    default = 256,     help = "Number of pixels of the image (must be the same dimension as the dimensions of the score model)")
-    
-
+    parser.add_argument("--npz_dir",            required = True,                                    help = "Path to the processed .npz measurement set")
+    parser.add_argument("--output_dir",         required = True,                                    help = "Directory where to save the gridded visibilities (specify a folder not any type of file)")
+    # parser.add_argument("--pixel_scale",        required = True, type = float,  default = 0.05,     help = "In arcsec")
+    parser.add_argument("--npix",               required = False, type = int,    default = 4096,    help = "Total number of pixels of the padded image.")
+    parser.add_argument("--window_function",    required = False, type = str,    default = "sinc",  help = "Either 'sinc' or 'pillbox'")
+    parser.add_argument("--img_size",           required = False, type = int,    default = 256,     help = "Number of pixels of the image (must be the same dimension as the dimensions of the score model)")
+    parser.add_argument("--debug_mode",         required = False, type = bool,   default = False,   help = "To debug the code, skip the gridding.")
     args = parser.parse_args()
     main(args) 
