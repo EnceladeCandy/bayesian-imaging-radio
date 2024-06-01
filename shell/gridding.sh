56aa9f5cf6b08367a6a21ae69c9ba20428e85182
EnceladeCandy <noe.dia@umontreal.ca>
2024-05-31 23:23:25 -0400

    Slightly changed script to correct a mistake on the Gridding of Elias20

--- shell/gridding.sh
+++ shell/gridding.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --tasks=1
-#SBATCH  --array=0-20%20
+#SBATCH  --array=0%1
 #SBATCH --cpus-per-task=3 # maximum cpu per task is 3.5 per gpus
 #SBATCH --mem=32G        # memory per node
 #SBATCH --time=00-02:59   # time (DD-HH:MM)
@@ -14,7 +14,7 @@ RESULTS_DIR=/home/noedia/scratch/bayesian_imaging_radio/dsharp_gridded
 SCRIPT_DIR=/home/noedia/projects/def-lplevass/noedia/bayesian_imaging_radio/bayesian-imaging-radio/src/preprocessing
 
 python $SCRIPT_DIR/gridding.py \
-    --npz_dir=$TARGETS_DIR \
+    --npz_dir=$TARGETS_DIR/Elias20_continuum.npz \
     --output_dir=$RESULTS_DIR \
     --npix=4096\
     --window_function="sinc"\
